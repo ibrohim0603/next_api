81b6f510ee6ac974ce11ae16e295f7ad356c44c4
ibrohim0603 <education0603@gmail.com>
2023-08-04 01:32:22 +0500

    eded

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import s from "./style.module.scss";
 import Link from "next/link";
 import cart from "../../../public/cart-plus-solid.svg";
+import Head from "next/head";
 
 interface Products {
   products: Det[];
@@ -34,45 +35,59 @@ export default function AllProducts() {
   console.log(mal?.products);
 
   return (
-    <div className={s.product}>
-      AllProducts
-      <div className={s.product_flex}>
-        {mal?.products.map((e) => (
-          <div key={e.id} className={s.product_card}>
-            <div className={s.product_img}>
-              <Image
-                src={e.img}
-                quality={100}
-                width={450}
-                height={250}
-                alt={e.name}
-              />
-            </div>
-            <div className={s.product_text}>
-              <h3>{e.name}</h3>
-              <p>
-                {e.title.length > 60 ? `${e.title.slice(0, 50)} ... ` : null}{" "}
-              </p>
-              <div className={s.product_price}>
-                <div>
-                  <h5>{e.price + e.price * 0.1} $</h5>
-                  <h3>{e.price} $</h3>
+    <>
+      <Head>
+        <title>Products page</title>
+        <meta name="description" content={`${mal?.products}`} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={s.product}>
+        AllProducts
+        <div className={s.product_flex}>
+          {mal?.products.map((e) => (
+            <Link href={`products/${e.id}`} key={e.id}>
+              <a>
+                <div className={s.product_card}>
+                  <div className={s.product_img}>
+                    <Image
+                      src={e.img}
+                      quality={100}
+                      width={450}
+                      height={250}
+                      alt={e.name}
+                    />
+                  </div>
+                  <div className={s.product_text}>
+                    <h3>{e.name}</h3>
+                    <p>
+                      {e.title.length > 60
+                        ? `${e.title.slice(0, 50)} ... `
+                        : e.title}{" "}
+                    </p>
+                    <div className={s.product_price}>
+                      <div>
+                        <h5>{e.price + e.price * 0.1} $</h5>
+                        <h3>{e.price} $</h3>
+                      </div>
+                      <div>
+                        <Link href={`/products/${e.id}`}>
+                          <a>Buy</a>
+                        </Link>
+                      </div>
+                    </div>
+                    <div className={s.product_view}>
+                      <Link href={`/products/${e.id}`}>
+                        <a>Увидеть продукт подробнее</a>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <Link href={`/single/${e.id}`}>
-                    <a>Buy</a>
-                  </Link>
-                </div>
-              </div>
-              <div className={s.product_view}>
-                <Link href={`/single/${e.id}`}>
-                  <a>Увидеть продукт подробнее</a>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
